c6b9aa59e2a3c50cdd108698461e6866103cdb8d
Ahmed Macan Markar <aamm723@uowmail.edu.au>
2022-08-29 23:26:30 +0400

    it done for now

---
+++ scorebord/index.js
@@ -0,0 +1,39 @@
+let homeEl=document.getElementById("homep")
+let guestEl=document.getElementById("guestp")
+let x=0
+let y=0
+function plus1_H(){
+    x+=1
+    homeEl.textContent=x
+    
+}
+
+function plus2_H(){
+    x+=2
+    homeEl.textContent=x
+    
+}
+
+function plus3_H(){
+    x+=3
+    homeEl.textContent=x
+    
+}
+
+function plus1_G(){
+    y+=1
+    guestEl.textContent=y
+    
+}
+
+function plus2_G(){
+    y+=2
+    guestEl.textContent=y
+    
+}
+
+function plus3_G(){
+    y+=3
+    guestEl.textContent=y
+    
+}
